f29061e72dc887070f81e64f759d22fdd7bd0af6
andreluizcolen <andrelcf.bh@gmail.com>
2021-07-03 14:21:52 -0300

    #1 Criação Classe Pessoa.kt

---
+++ fundamentos_oo/src/main/kotlin/br/com/dnadopet/cursobank/Pessoa.kt
@@ -0,0 +1,14 @@
+package br.com.dnadopet.cursobank
+
+class Pessoa {
+    var nome:String = "André Luiz"
+    var cpf:String = "123.456.789-01"
+
+}
+
+fun main() {
+
+    val jether = Pessoa()
+    println("Nome " + jether.nome + " Cpf: " + jether.cpf )
+
+}
