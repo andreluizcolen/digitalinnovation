0b788d81ce08c415a2997663d6e334eda1c2758f
andreluizcolen <andrelcf.bh@gmail.com>
2021-07-03 18:24:39 -0300

    #1 Criação Classe PessoaInnerClass.kt

---
+++ fundamentos_oo/src/main/kotlin/br/com/dnadopet/cursobank/PessoaInnerClass.kt
@@ -0,0 +1,18 @@
+package br.com.dnadopet.cursobank
+
+class PessoaInnerClass {
+
+        var nome:String = "André Luiz Colen"
+        var cpf:String = "123.456.789-01"
+        inner class Endereco {
+            var rua:String ="Rua Monte Castelo"
+            var numero:String="Número 123"
+        }
+}
+
+fun main() {
+
+    val jether = PessoaInnerClass()
+    println("Nome " + jether.nome + " Cpf:  " + jether.cpf  + "Endereço: " + jether.Endereco().rua)
+
+}
